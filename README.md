84fa9fd3dd6509637d28c045e58d8d83fdc3791b
rooooolin <rooolin.s@gmail.com>
2023-06-14 11:54:20 +0800

    0614

---
+++ README.md
@@ -0,0 +1,13 @@
+
+a sample tools for convert .doc to .docx on windows
+
+## Usage
+
+
+```
+python doc2docx /folder/to/.doc
+```
+
+#### Note
+
+if a Word pop-up appears, please manually click on cancel, close or do not save, etc.
---
+++ doc2docx.py
@@ -0,0 +1,45 @@
+
+import os
+import win32com.client as win32
+import argparse
+
+
+'''
+pywin32                   302              py39h2bbff1b_2
+pywin32-ctypes            0.2.0           py39haa95532_1000
+pywinpty                  2.0.2            py39h5da7b33_0
+'''
+def find_files(dir_path, file_ext=["docx"]):
+    file_list = []
+    for root, dirs, files in os.walk(dir_path):
+        for filename in files:
+            if any(filename.endswith(ext) for ext in file_ext):
+                if not os.path.basename(filename).startswith('~$'):
+                    file_list.append(os.path.join(root, filename))
+    return file_list
+
+def parse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('folder', default='',help='')
+    args = parser.parse_args()
+    return args
+
+def run():
+    args = parse()
+    docs = find_files(args.folder)
+    for i,doc in enumerate(docs):
+        try:
+            file_name = os.path.basename(doc)
+            if not file_name.startswith('~$'):
+                docx = doc.replace(".doc",".docx")
+                if not os.path.isfile(docx):
+                    print(f"[{i+1}/{len(docs)}]:{doc}")
+                    application = win32.DispatchEx('Word.Application')
+                    word = application.Documents.Open(doc)
+                    word.SaveAs2(docx, FileFormat=16)
+                    application.Quit()
+        except Exception as e:
+            print(e)
+    
+if __name__=='__main__':
+    run()
